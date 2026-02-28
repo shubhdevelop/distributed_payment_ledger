321393aa424540edae240d662639cf5c1ba176db
shubham <shubhamxraj10@gmail.com>
2026-03-01 01:42:38 +0530

    feat: Implement Valkey integration with a basic HTTP transfer endpoint and configure the development environment with Air.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,39 @@
 // Package main
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/shubhdevelop/distributed_payment_ledger/internal/valkey"
+)
 
 func main() {
 	fmt.Println("hello world")
+	ctx := context.Background()
+	valkeyClient, err := valkey.ValkeyInit(ctx, "localhost", 6379)
+	if err != nil {
+		fmt.Printf("Error connecting to Valkey: %w", err)
+	}
+	err = valkey.LoadValkeyScripts(ctx, valkeyClient)
+	if err != nil {
+		fmt.Printf("Error loading scripts to Valkey: %w", err)
+	}
+	router := http.NewServeMux()
+	router.HandleFunc("POST /transfer", func(r http.ResponseWriter, w *http.Request) {
+		err := valkey.Touch(ctx, "example_key", valkeyClient)
+		if err != nil {
+			fmt.Println("error ping redis")
+		}
+		_, err = r.Write([]byte("hello transfered!!"))
+		if err != nil {
+			fmt.Printf("error while writing to the r.Writer: %w", err)
+		}
+	})
+
+	err = http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Printf("error Starting the server: %w", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/shubhdevelop/distributed_payment_ledger
 
-go 1.25.5
+go 1.24
+
+require (
+	github.com/valkey-io/valkey-glide/go/v2 v2.2.7 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
